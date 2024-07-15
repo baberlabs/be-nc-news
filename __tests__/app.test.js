116d6f33b83f7ff791f05794bdc46b538c206934
Baber Khan <bbrkhnn@outlook.com>
2024-07-15 11:54:35 +0100

    Get all topics
    - package.json: Added supertest as dev dependency and express as normal dependency
    - __tests__/app.test.js: Added test for GET /api/topics endpoint
    - app.js: Created server and handled GET /api/topics endpoint
    - controllers/topics.controllers.js: Created getTopics() controller function that will get all topics from the fetchTopics() model function and serve it to GET /api/topics endpoint
    - models/topics.models.js: Created fetchTopics() model function that will fetch all topics from the database
    - controllers/errors.controllers.js: Added serverErrorsHandler() controller function

---
+++ __tests__/app.test.js
@@ -0,0 +1,27 @@
+const request = require("supertest");
+const app = require("../app");
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test-data/");
+const db = require("../db/connection");
+
+beforeEach(() => seed(testData));
+afterAll(() => db.end());
+
+describe("/api/topics", () => {
+  test("GET:200 get all topics", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body: { topics } }) => {
+        expect(Array.isArray(topics)).toBe(true);
+        expect(topics.length).toBe(3);
+        console.log(topics);
+        topics.forEach((topic) => {
+          expect(topic).toMatchObject({
+            slug: expect.any(String),
+            description: expect.any(String),
+          });
+        });
+      });
+  });
+});
