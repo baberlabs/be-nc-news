20cb51c2599701496ed0f699c31bbd5999959170
Baber Khan <bbrkhnn@outlook.com>
2024-08-05 09:57:11 +0100

    allow cross origin resource sharing (cors)

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const {
 } = require("./controllers/errors.controllers");
 
 const express = require("express");
+const cors = require("cors");
 
 const apiRouter = require("./routes/api-router");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
